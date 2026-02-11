b47bc06b13525c9d770e36d42b98f8855c2a7f86
JoaoMafra07 <jrlorimafra@gmail.com>
2026-02-11 11:10:42 -0300

    Primeira aula de Dart/Flutter

---
+++ aula1/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TelaInicial());
+}
+
+class TelaInicial extends StatelessWidget {
+  const TelaInicial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
